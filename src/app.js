ad864bbcec5e6479e041fa810a502142d27e301b
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-12-09 21:18:50 -0500

    added some route files and data file

--- src/app.js
+++ src/app.js
@@ -1,33 +1,56 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const winston = require('winston');
+const { NODE_ENV } = require('./config');
+const bookmarkRoutes = require('./bookmarkRoutes');
 
-const app = express()
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({filename: 'info.log'})
+  ]
+});
+
+if (NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+
+const app = express();
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+//middleware
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
 
-app.get('/', (req,res) => {
-    res.send('Hello, world!')
-})
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path ${req.path}`);
+    return res.status(401).json({error: 'Unauthorized request'});
+  }
+  next();
+});
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+app.use(bookmarkRoutes);
 
 app.use(function errorHandler(error, req, res, next) {
-       let response
-       if (NODE_ENV === 'production'){
-         response = { error: { message: 'server error' } }
-       } else {
-         console.error(error)
-         response = { message: error.message, error }
-       }
-       res.status(500).json(response)
-     })
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
 
-module.exports = app
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
-const app = require('./app')
-
-// const PORT = process.env.PORT || 8000
-const { PORT } = require('./config')
+const app = require('./app');
+const {PORT} = require('./config');
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+  console.log(`Server is listening at http://localhost:${PORT}`);
+});
